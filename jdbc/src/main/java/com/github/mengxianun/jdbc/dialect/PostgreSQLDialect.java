3b8bf933ec8d681f750da2c834821d8b0001d89e
mengxiangyun <718517636@qq.com>
2019-04-20 18:54:24 +0800

    feat: 添加PostgreSQL方言支持

---
+++ jdbc/src/main/java/com/github/mengxianun/jdbc/dialect/PostgreSQLDialect.java
@@ -0,0 +1,29 @@
+package com.github.mengxianun.jdbc.dialect;
+
+import com.github.mengxianun.core.Dialect;
+import com.google.auto.service.AutoService;
+
+@AutoService(Dialect.class)
+public class PostgreSQLDialect implements Dialect {
+
+	@Override
+	public String getType() {
+		return "postgresql";
+	}
+
+	@Override
+	public boolean assignDatabase() {
+		return false;
+	}
+
+	@Override
+	public boolean quoteTable() {
+		return false;
+	}
+
+	@Override
+	public String getJsonPlaceholder() {
+		return "?::json";
+	}
+
+}
