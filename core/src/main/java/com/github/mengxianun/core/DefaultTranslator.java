118a7a4532d564d7844a388576d2a0da5f115bf6
mengxiangyun <718517636@qq.com>
2019-07-09 12:00:56 +0800

    refactor: Code optimization

--- core/src/main/java/com/github/mengxianun/core/DefaultTranslator.java
+++ core/src/main/java/com/github/mengxianun/core/DefaultTranslator.java
@@ -1,11 +1,9 @@
 package com.github.mengxianun.core;
 
 import java.net.URL;
-import java.time.Duration;
 
 import com.github.mengxianun.core.attributes.ConfigAttributes;
 import com.github.mengxianun.core.resutset.DefaultDataResultSet;
-import com.google.common.base.Stopwatch;
 import com.google.gson.JsonElement;
 
 public class DefaultTranslator extends AbstractTranslator {
@@ -25,12 +23,11 @@ public class DefaultTranslator extends AbstractTranslator {
 
 	@Override
 	public DataResultSet translate(String json) {
-		Stopwatch stopwatch = Stopwatch.createStarted();
+
 		Executor executor = App.getInjector().getInstance(Executor.class);
 		JsonElement result = executor.execute(json);
 
-		Duration duration = stopwatch.stop().elapsed();
-		return new DefaultDataResultSet(duration.toMillis(), result);
+		return new DefaultDataResultSet(0L, result);
 
 	}
 
