470bab837ccea005686fea2ae20b5b0e54195503
mengxiangyun <718517636@qq.com>
2019-07-09 16:24:26 +0800

    feat: 连接池添加wall过滤器

--- jdbc/src/main/java/com/github/mengxianun/jdbc/JdbcDataContextFactory.java
+++ jdbc/src/main/java/com/github/mengxianun/jdbc/JdbcDataContextFactory.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 import com.alibaba.druid.pool.DruidDataSourceFactory;
 import com.github.mengxianun.core.DataContextFactory;
 import com.google.auto.service.AutoService;
+import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
@@ -22,6 +23,20 @@ public final class JdbcDataContextFactory implements DataContextFactory {
 
 	@Override
 	public JdbcDataContext create(JsonObject dataSourceJsonObject) {
+		// 添加 wall 过滤器, 防止 SQL 注入
+		String filters = "filters";
+		String wall = "wall";
+		if (dataSourceJsonObject.has(filters)) {
+			String filtersString = dataSourceJsonObject.get(filters).getAsString();
+			if (Strings.isNullOrEmpty(filtersString)) {
+				dataSourceJsonObject.addProperty(filters, wall);
+			} else if (!filtersString.contains(wall)) {
+				filtersString += "," + wall;
+				dataSourceJsonObject.addProperty(filters, filtersString);
+			}
+		} else {
+			dataSourceJsonObject.addProperty(filters, wall);
+		}
 		Type type = new TypeToken<Map<String, String>>() {
 		}.getType();
 		Map<String, String> dataSourceMap = new Gson().fromJson(dataSourceJsonObject, type);
