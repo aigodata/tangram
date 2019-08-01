a9a39304b63f4cc26c3d17ba3bcc14700eae2e74
mengxiangyun <718517636@qq.com>
2019-08-01 19:55:20 +0800

    style: add serialVersionUID

--- elasticsearch/src/main/java/com/github/mengxianun/elasticsearch/schema/ElasticsearchColumn.java
+++ elasticsearch/src/main/java/com/github/mengxianun/elasticsearch/schema/ElasticsearchColumn.java
@@ -3,5 +3,7 @@ package com.github.mengxianun.elasticsearch.schema;
 import com.github.mengxianun.core.schema.DefaultColumn;
 
 public class ElasticsearchColumn extends DefaultColumn {
+
+	private static final long serialVersionUID = 1L;
 	
 }
