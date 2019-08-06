b3ce48c23e94d362938c2d9b77886b97b33d891f
mengxiangyun <718517636@qq.com>
2019-08-06 14:08:15 +0800

    feat: Elasticsearch Test

---
+++ elasticsearch/src/test/java/com/github/mengxianun/elasticsearch/SelectTest.java
@@ -0,0 +1,173 @@
+package com.github.mengxianun.elasticsearch;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.github.mengxianun.core.DataResultSet;
+import com.github.mengxianun.core.config.ResultAttributes;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+@DisplayName("Elasticsearch select test")
+public class SelectTest extends TestSupport {
+
+	private static final String JSON_PARENT_PATH = "json/select/";
+
+	@Test
+	void testTable() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_table.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(6, result.size());
+	}
+
+	@Test
+	void testSourceTable() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_source_table.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertTrue(result.size() > 0);
+	}
+
+	@Test
+	void testWhereEqual() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_equal.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(1, result.size());
+	}
+
+	@Test
+	void testWhereNotEqual() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_not_equal.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(5, result.size());
+	}
+
+	@Test
+	void testWhereLike() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_like.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(2, result.size());
+	}
+
+	@Test
+	void testWhereGreater() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_greater.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(3, result.size());
+	}
+
+	@Test
+	void testWhereGreaterEqual() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_greater_equal.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(4, result.size());
+	}
+
+	@Test
+	void testWhereLess() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_less.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(1, result.size());
+	}
+
+	@Test
+	void testWhereLessEqual() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_less_equal.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(2, result.size());
+	}
+
+	@Test
+	void testWhereBetween() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_between.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(3, result.size());
+	}
+
+	@Test
+	void testWhereIn() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_in.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(3, result.size());
+	}
+
+	@Test
+	void testWhereNotIn() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_not_in.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(3, result.size());
+	}
+
+	@Test
+	void testWhereAnd() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_and.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(1, result.size());
+	}
+
+	@Test
+	void testWhereOr() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_or.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(2, result.size());
+	}
+
+	@Test
+	void testWhereAndOr() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_and_or.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(2, result.size());
+	}
+
+	@Test
+	void testWhereComplex() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_where_complex.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(2, result.size());
+	}
+
+	@Test
+	void testGroup() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_group.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(5, result.size());
+	}
+
+	@Test
+	void testOrder() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_order.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertEquals(6, result.size());
+		JsonObject firstElement = result.get(0).getAsJsonObject();
+		String maxAge = firstElement.get("age").getAsString();
+		assertEquals("99.0", maxAge);
+		JsonObject lastElement = result.get(result.size() - 1).getAsJsonObject();
+		String minAge = lastElement.get("age").getAsString();
+		assertEquals("10.0", minAge);
+	}
+	
+	@Test
+	void testLimit() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_limit.json");
+		JsonObject result = (JsonObject) dataResultSet.getJsonData();
+		assertTrue(result.has(ResultAttributes.TOTAL));
+		long total = result.get(ResultAttributes.TOTAL).getAsLong();
+		assertEquals(6, total);
+		JsonArray data = result.get(ResultAttributes.DATA).getAsJsonArray();
+		assertEquals(2, data.size());
+	}
+
+	@Test
+	void testAlias() {
+		DataResultSet dataResultSet = run(JSON_PARENT_PATH + "select_alias.json");
+		JsonArray result = (JsonArray) dataResultSet.getJsonData();
+		assertTrue(result.size() > 0);
+		JsonObject data = result.get(0).getAsJsonObject();
+		assertTrue(data.has("aid"));
+		assertTrue(data.has("A-name"));
+		assertTrue(data.has("age"));
+	}
+
+}
---
+++ elasticsearch/src/test/java/com/github/mengxianun/elasticsearch/TestSupport.java
@@ -0,0 +1,109 @@
+package com.github.mengxianun.elasticsearch;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy;
+import org.elasticsearch.client.IndicesClient;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.jupiter.api.BeforeAll;
+
+import com.github.mengxianun.core.DataResultSet;
+import com.github.mengxianun.core.DefaultTranslator;
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+public class TestSupport {
+
+	static final Logger LOG = Logger.getLogger(TestSupport.class.getName());
+	
+	private static final String TEST_INDEX = "test";
+	private static final String TEST_DATA = "index.json";
+	private static final String TEST_CONFIG_FILE = "test.json";
+	public static final DefaultTranslator translator;
+
+	static {
+		translator = new DefaultTranslator(TEST_CONFIG_FILE);
+		translator.addFactory(new ElasticsearchDataContextFactory());
+		translator.reInit();
+	}
+
+	@BeforeAll
+	public static void init() {
+		// Initialize test data
+		RestClientBuilder clientBuilder = RestClient.builder(new HttpHost("localhost", 9200, "http"));
+		try (RestHighLevelClient client = new RestHighLevelClient(clientBuilder)) {
+			IndicesClient indices = client.indices();
+			// Delete
+			GetIndexRequest getIndexRequest = new GetIndexRequest(TEST_INDEX);
+			boolean exists = indices.exists(getIndexRequest, RequestOptions.DEFAULT);
+			if (exists) {
+				DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest(TEST_INDEX);
+				indices.delete(deleteIndexRequest, RequestOptions.DEFAULT);
+			}
+			// Create
+			CreateIndexRequest createIndexRequest = new CreateIndexRequest(TEST_INDEX);
+			indices.create(createIndexRequest, RequestOptions.DEFAULT);
+
+			String dataString = Resources.toString(Resources.getResource(TEST_DATA), StandardCharsets.UTF_8);
+			JsonArray dataArray = new Gson().fromJson(dataString, JsonArray.class);
+			BulkRequest bulkRequest = new BulkRequest();
+			for (JsonElement jsonElement : dataArray) {
+				String source = jsonElement.getAsJsonObject().toString();
+				IndexRequest indexRequest = new IndexRequest(TEST_INDEX, "_doc");
+				indexRequest.source(source, XContentType.JSON);
+				bulkRequest.add(indexRequest);
+			}
+			bulkRequest.setRefreshPolicy(RefreshPolicy.IMMEDIATE);
+			BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+			int length = bulkResponse.getItems().length;
+
+			LOG.info("Initialize " + length + " elasticsearch test data");
+		} catch (IOException e) {
+			LOG.log(Level.SEVERE, "Elasticsearch create index failed", e);
+		}
+
+	}
+
+	String readJson(String jsonFile) {
+		URL url = Resources.getResource(jsonFile);
+		try {
+			return Resources.toString(url, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			LOG.warning(e.getMessage());
+			return "";
+		}
+	}
+
+	DataResultSet run(String jsonFile) {
+		return runJson(readJson(jsonFile));
+	}
+
+	DataResultSet runJson(String json) {
+		DataResultSet dataResultSet = translator.translate(json);
+		LOG.info("-----------------Json-----------------");
+		LOG.info(json);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		LOG.info("-----------------Result-----------------");
+		LOG.info(gson.toJson(dataResultSet));
+		return dataResultSet;
+	}
+
+}
