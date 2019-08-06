431c441f45ff651e00313006c236b367214b4f3d
mengxiangyun <718517636@qq.com>
2019-08-06 14:55:49 +0800

    feat: 添加通过url,username,password创建DataSource方法

--- jdbc/src/main/java/com/github/mengxianun/jdbc/JdbcDataContextFactory.java
+++ jdbc/src/main/java/com/github/mengxianun/jdbc/JdbcDataContextFactory.java
@@ -1,6 +1,7 @@
 package com.github.mengxianun.jdbc;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.sql.DataSource;
@@ -23,30 +24,40 @@ public final class JdbcDataContextFactory implements DataContextFactory {
 
 	@Override
 	public JdbcDataContext create(JsonObject dataSourceJsonObject) {
-		// 添加 wall 过滤器, 防止 SQL 注入
-		String filters = "filters";
-		String wall = "wall";
-		if (dataSourceJsonObject.has(filters)) {
-			String filtersString = dataSourceJsonObject.get(filters).getAsString();
-			if (Strings.isNullOrEmpty(filtersString)) {
-				dataSourceJsonObject.addProperty(filters, wall);
-			} else if (!filtersString.contains(wall)) {
-				filtersString += "," + wall;
-				dataSourceJsonObject.addProperty(filters, filtersString);
-			}
-		} else {
-			dataSourceJsonObject.addProperty(filters, wall);
-		}
 		Type type = new TypeToken<Map<String, String>>() {
 		}.getType();
-		Map<String, String> dataSourceMap = new Gson().fromJson(dataSourceJsonObject, type);
-		DataSource druidDataSource;
+		Map<String, Object> dataSourceMap = new Gson().fromJson(dataSourceJsonObject, type);
+		DataSource dataSource = createDataSource(dataSourceMap);
+		return new JdbcDataContext(dataSource);
+	}
+
+	public DataSource createDataSource(String url, String username, String password) {
+		Map<String, Object> dataSourceMap = new HashMap<>();
+		dataSourceMap.put(DruidDataSourceFactory.PROP_URL, url);
+		dataSourceMap.put(DruidDataSourceFactory.PROP_USERNAME, username);
+		dataSourceMap.put(DruidDataSourceFactory.PROP_PASSWORD, password);
+		return createDataSource(dataSourceMap);
+	}
+
+	public DataSource createDataSource(Map<String, Object> dataSourceMap) {
+		// Add wall filters to prevent SQL injection
+		String wall = "wall";
+		if (dataSourceMap.containsKey(DruidDataSourceFactory.PROP_FILTERS)) {
+			String filtersString = dataSourceMap.get(DruidDataSourceFactory.PROP_FILTERS).toString();
+			if (Strings.isNullOrEmpty(filtersString)) {
+				dataSourceMap.put(DruidDataSourceFactory.PROP_FILTERS, wall);
+			} else if (!filtersString.contains(wall)) {
+				filtersString += "," + wall;
+				dataSourceMap.put(DruidDataSourceFactory.PROP_FILTERS, filtersString);
+			}
+		} else {
+			dataSourceMap.put(DruidDataSourceFactory.PROP_FILTERS, wall);
+		}
 		try {
-			druidDataSource = DruidDataSourceFactory.createDataSource(dataSourceMap);
+			return DruidDataSourceFactory.createDataSource(dataSourceMap);
 		} catch (Exception e) {
 			throw new JdbcDataException(e);
 		}
-		return new JdbcDataContext(druidDataSource);
 	}
 
 }
