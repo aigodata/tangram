125e59800d46adc655c57d9a8df7e1e0d8db6685
mengxiangyun <718517636@qq.com>
2019-08-07 21:48:08 +0800

    feat: 关键字

---
+++ core/src/main/java/com/github/mengxianun/core/Keyword.java
@@ -0,0 +1,12 @@
+package com.github.mengxianun.core;
+
+
+public final class Keyword {
+
+	private Keyword() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	public static final String DATE_NOW = "$now";
+
+}
--- core/src/main/java/com/github/mengxianun/core/item/Item.java
+++ core/src/main/java/com/github/mengxianun/core/item/Item.java
@@ -5,8 +5,10 @@ import java.sql.Timestamp;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import com.github.mengxianun.core.Keyword;
 import com.github.mengxianun.core.schema.Column;
 import com.github.mengxianun.core.schema.ColumnType;
 
@@ -21,27 +23,11 @@ public abstract class Item implements Serializable {
 		if (value.getClass().isArray()) {
 			return getRealValueInArray(column, value);
 		}
+		if (Keyword.DATE_NOW.equals(value)) {
+			return LocalDateTime.now();
+		}
 		if (column != null) {
-			ColumnType columnType = column.getType();
-			if (columnType.isLiteral()) {
-				return String.valueOf(value);
-			} else if (columnType.isNumber()) {
-				try {
-					return NumberFormat.getInstance().parse(value.toString());
-				} catch (ParseException e) {
-					// ignore
-				}
-			} else if (columnType.isTimeBased()) {
-				if (columnType.isDate()) {
-					return LocalDate.parse(value.toString());
-				} else if (columnType.isTime()) {
-					return LocalTime.parse(value.toString());
-				} else if (columnType.isTimestamp()) {
-					return Timestamp.valueOf(value.toString());
-				}
-			} else if (columnType.isBoolean()) {
-				return Boolean.parseBoolean(value.toString());
-			}
+			value = getRealValue(column.getType(), value);
 		}
 		return value;
 	}
@@ -59,6 +45,27 @@ public abstract class Item implements Serializable {
 				realValueArray[i] = getRealValue(column, ogValueArray[i]);
 			}
 			return realValueArray;
+		}
+		return value;
+	}
+
+	private Object getRealValue(ColumnType columnType, Object value) {
+		if (columnType.isLiteral()) {
+			return String.valueOf(value);
+		} else if (columnType.isNumber()) {
+			try {
+				return NumberFormat.getInstance().parse(value.toString());
+			} catch (ParseException ignore) {}
+		} else if (columnType.isTimeBased()) {
+			if (columnType.isDate()) {
+				return LocalDate.parse(value.toString());
+			} else if (columnType.isTime()) {
+				return LocalTime.parse(value.toString());
+			} else if (columnType.isTimestamp()) {
+				return Timestamp.valueOf(value.toString());
+			}
+		} else if (columnType.isBoolean()) {
+			return Boolean.parseBoolean(value.toString());
 		}
 		return value;
 	}
