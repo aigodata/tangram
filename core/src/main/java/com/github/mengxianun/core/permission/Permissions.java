3e1dae7dedbfb67a8bdb7567afb28ade5f313104
mengxiangyun <718517636@qq.com>
2019-11-04 15:44:37 +0800

    docs: add java doc

--- core/src/main/java/com/github/mengxianun/core/permission/Permissions.java
+++ core/src/main/java/com/github/mengxianun/core/permission/Permissions.java
@@ -21,7 +21,7 @@ import com.google.common.base.Strings;
 import com.google.gson.JsonObject;
 
 /**
- * 权限工具类, 基于当前用户
+ * Permission tool class, based on the current user
  * 
  * @author mengxiangyun
  *
@@ -32,46 +32,122 @@ public class Permissions {
 		throw new AssertionError();
 	}
 
+	/**
+	 * Whether the current user has table permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTablePermission(String table) {
 		return hasTablePermission(null, table);
 	}
 
+	/**
+	 * Whether the current user has table permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTablePermission(String source, String table) {
 		return App.hasTablePermissions(source, table);
 	}
 
+	/**
+	 * Whether the current user has table select permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTableSelectPermission(String table) {
 		return hasTableSelectPermission(null, table);
 	}
 
+	/**
+	 * Whether the current user has table select permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTableSelectPermission(String source, String table) {
 		return hasTableActionPermission(source, table, Action.SELECT);
 	}
 
+	/**
+	 * Whether the current user has table insert permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTableInsertPermission(String table) {
 		return hasTableInsertPermission(null, table);
 	}
 
+	/**
+	 * Whether the current user has table insert permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTableInsertPermission(String source, String table) {
 		return hasTableActionPermission(source, table, Action.INSERT);
 	}
 
+	/**
+	 * Whether the current user has table update permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTableUpdatePermission(String table) {
 		return hasTableUpdatePermission(null, table);
 	}
 	
+	/**
+	 * Whether the current user has table update permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTableUpdatePermission(String source, String table) {
 		return hasTableActionPermission(source, table, Action.UPDATE);
 	}
 
+	/**
+	 * Whether the current user has table delete permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTableDeletePermission(String table) {
 		return hasTableDeletePermission(null, table);
 	}
 
+	/**
+	 * Whether the current user has table delete permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static boolean hasTableDeletePermission(String source, String table) {
 		return hasTableActionPermission(source, table, Action.DELETE);
 	}
 
+	/**
+	 * Whether the current user has table action permissions
+	 * 
+	 * @param source
+	 *            data source
+	 * @param table
+	 *            source table
+	 * @param action
+	 *            operation action
+	 * @return
+	 */
 	private static boolean hasTableActionPermission(String source, String table, Action action) {
 		if (hasTablePermission(source, table)) {
 			List<TablePermission> tablePermissions = App.getTablePermissions(source, table);
@@ -81,10 +157,23 @@ public class Permissions {
 		return false;
 	}
 
+	/**
+	 * Get the current user's table permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTablePermissions(String table) {
 		return getTablePermissions(null, table);
 	}
 
+	/**
+	 * Get the current user's table permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTablePermissions(String source, String table) {
 		List<TablePermission> tablePermissions = App.getTablePermissions(source, table);
 		List<ConnectorCondition> conditions = tablePermissions.stream().flatMap(e -> e.conditions().stream())
@@ -92,38 +181,98 @@ public class Permissions {
 		return new TablePermissions(source, table, conditions);
 	}
 
+	/**
+	 * Get the current user's table select permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTableSelectPermissions(String table) {
 		return getTableSelectPermissions(null, table);
 	}
 
+	/**
+	 * Get the current user's table select permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTableSelectPermissions(String source, String table) {
 		return getTableActionPermissions(source, table, Action.SELECT);
 	}
 
+	/**
+	 * Get the current user's table insert permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTableInsertPermissions(String table) {
 		return getTableInsertPermissions(null, table);
 	}
 
+	/**
+	 * Get the current user's table insert permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTableInsertPermissions(String source, String table) {
 		return getTableActionPermissions(source, table, Action.INSERT);
 	}
 
+	/**
+	 * Get the current user's table update permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTableUpdatePermissions(String table) {
 		return getTableUpdatePermissions(null, table);
 	}
 
+	/**
+	 * Get the current user's table update permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTableUpdatePermissions(String source, String table) {
 		return getTableActionPermissions(source, table, Action.UPDATE);
 	}
 
+	/**
+	 * Get the current user's table delete permissions
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTableDeletePermissions(String table) {
 		return getTableDeletePermissions(null, table);
 	}
 
+	/**
+	 * Get the current user's table delete permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static TablePermissions getTableDeletePermissions(String source, String table) {
 		return getTableActionPermissions(source, table, Action.DELETE);
 	}
 
+	/**
+	 * Get the current user's table action permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @param action
+	 * @return
+	 */
 	private static TablePermissions getTableActionPermissions(String source, String table, Action action) {
 		List<TablePermission> tablePermissions = App.getTablePermissions(source, table);
 		List<ConnectorCondition> conditions = tablePermissions.stream()
@@ -133,14 +282,35 @@ public class Permissions {
 		return new TablePermissions(source, table, conditions);
 	}
 
+	/**
+	 * Get the current user's table permissions where
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static String getWhere(String table) {
 		return getWhere(null, table);
 	}
 
+	/**
+	 * Get the current user's table permissions where
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static String getWhere(String source, String table) {
 		return getTablePermissions(source, table).toSQL();
 	}
 
+	/**
+	 * Get the current user's table permissions condition sql
+	 * 
+	 * @param source
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static String getTableConditionSessionSql(String source, String table, String column) {
 		AuthorizationInfo authorizationInfo = App.getAuthorizationInfo();
 		String userTable = authorizationInfo.getUserTable();
@@ -164,6 +334,12 @@ public class Permissions {
 		}
 	}
 
+	/**
+	 * Condition to SQL
+	 * 
+	 * @param condition
+	 * @return
+	 */
 	private static String toConditionSQL(Condition condition) {
 		StringBuilder builder = new StringBuilder();
 		if (condition instanceof TableCondition) {
@@ -236,46 +412,130 @@ public class Permissions {
 
 	}
 
+	/**
+	 * Whether the current user has column permissions
+	 * 
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnPermission(String table, String column) {
 		return hasColumnPermission(null, table, column);
 	}
 
+	/**
+	 * Whether the current user has column permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnPermission(String source, String table, String column) {
 		return App.hasColumnPermissions(source, table, column);
 	}
 
+	/**
+	 * Whether the current user has column select permissions
+	 * 
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnSelectPermission(String table, String column) {
 		return hasColumnSelectPermission(null, table, column);
 	}
 
+	/**
+	 * Whether the current user has column select permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnSelectPermission(String source, String table, String column) {
 		return hasColumnActionPermission(source, table, column, Action.SELECT);
 	}
 
+	/**
+	 * Whether the current user has column insert permissions
+	 * 
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnInsertPermission(String table, String column) {
 		return hasColumnInsertPermission(null, table, column);
 	}
 
+	/**
+	 * Whether the current user has column insert permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnInsertPermission(String source, String table, String column) {
 		return hasColumnActionPermission(source, table, column, Action.INSERT);
 	}
 
+	/**
+	 * Whether the current user has column update permissions
+	 * 
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnUpdatePermission(String table, String column) {
 		return hasColumnUpdatePermission(null, table, column);
 	}
 
+	/**
+	 * Whether the current user has column update permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnUpdatePermission(String source, String table, String column) {
 		return hasColumnActionPermission(source, table, column, Action.UPDATE);
 	}
 
+	/**
+	 * Whether the current user has column delete permissions
+	 * 
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnDeletePermission(String table, String column) {
 		return hasColumnDeletePermission(null, table, column);
 	}
 
+	/**
+	 * Whether the current user has column delete permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @param column
+	 * @return
+	 */
 	public static boolean hasColumnDeletePermission(String source, String table, String column) {
 		return hasColumnActionPermission(source, table, column, Action.DELETE);
 	}
 
+	/**
+	 * Whether the current user has column action permissions
+	 * 
+	 * @param source
+	 * @param table
+	 * @param column
+	 * @param action
+	 * @return
+	 */
 	private static boolean hasColumnActionPermission(String source, String table, String column, Action action) {
 		if (hasColumnPermission(source, table, column)) {
 			List<ColumnPermission> columnPermissions = App.getColumnPermissions(source, table, column);
@@ -284,10 +544,23 @@ public class Permissions {
 		return false;
 	}
 
+	/**
+	 * Get the current user's permissions columns
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getPermissionColumns(String table) {
 		return getPermissionColumns(null, table);
 	}
 
+	/**
+	 * Get the current user's permissions columns
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getPermissionColumns(String source, String table) {
 		if (Strings.isNullOrEmpty(source)) {
 			source = App.getDefaultDataSource();
@@ -310,38 +583,98 @@ public class Permissions {
 		return columnNames;
 	}
 
+	/**
+	 * Get the current user's select permissions columns
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getSelectColumns(String table) {
 		return getSelectColumns(null, table);
 	}
 
+	/**
+	 * Get the current user's select permissions columns
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getSelectColumns(String source, String table) {
 		return getActionColumns(source, table, Action.SELECT);
 	}
 
+	/**
+	 * Get the current user's insert permissions columns
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getInsertColumns(String table) {
 		return getInsertColumns(null, table);
 	}
 
+	/**
+	 * Get the current user's insert permissions columns
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getInsertColumns(String source, String table) {
 		return getActionColumns(source, table, Action.INSERT);
 	}
 
+	/**
+	 * Get the current user's update permissions columns
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getUpdateColumns(String table) {
 		return getUpdateColumns(null, table);
 	}
 
+	/**
+	 * Get the current user's update permissions columns
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getUpdateColumns(String source, String table) {
 		return getActionColumns(source, table, Action.UPDATE);
 	}
 
+	/**
+	 * Get the current user's delete permissions columns
+	 * 
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getDeleteColumns(String table) {
 		return getDeleteColumns(null, table);
 	}
 
+	/**
+	 * Get the current user's delete permissions columns
+	 * 
+	 * @param source
+	 * @param table
+	 * @return
+	 */
 	public static List<String> getDeleteColumns(String source, String table) {
 		return getActionColumns(source, table, Action.DELETE);
 	}
 
+	/**
+	 * Get the current user's action permissions columns
+	 * 
+	 * @param source
+	 * @param table
+	 * @param action
+	 * @return
+	 */
 	private static List<String> getActionColumns(String source, String table, Action action) {
 		Table sourceTable = App.getTable(source, table);
 		if (sourceTable == null) {
